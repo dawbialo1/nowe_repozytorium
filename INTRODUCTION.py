d6aafa405d5dfeb022319ca32a3d30eb8e4599ec
dawbialo1 <dawbialo1@op.pl>
2020-09-19 13:54:36 +0200

    moj_plik

---
+++ INTRODUCTION.py
@@ -0,0 +1,93 @@
+print("Hello World!")
+print ("spam and eggs")
+print (int(3/4))
+print (8 / 2)
+print (6 * 7.0)
+print (4 + 1.65)
+print (2**5)
+print (9 ** (1/2))
+print (22 // 6)
+print (24 // 6)
+print (24.45 // 3)
+print (20 % 6)
+print (20.14 % 6)
+print (1.25 % 0.5)
+print (7 % 5)
+
+print ()
+print ('Python in my favourite language')
+print ('Brian\'s mother and \'adjika\'')
+print ("The most popular bus company in Hungary called \"IKARUS\"")
+
+print ("""abcde fgd dfd """)
+print ('one \n two \n three')
+print ("one \n two\t three")
+print ()
+
+print ("spam" + 'eggs')
+print ("2" + "2")
+print (2 + 2)
+print ("spam" * 3)
+
+print()
+
+user = "James"
+print(user)
+
+x=8
+print(x)
+print(x+3)
+print(x)
+
+y="Kasia"
+print(y + "!")
+
+this_is_my_name = "Dawid"
+print(this_is_my_name)
+
+foo = 3
+print(foo)
+
+foo = 3
+bar = 2 
+print(bar)
+del bar
+bar = 8
+print (bar + foo)
+
+# q = input("Enter your name")
+# print("HELLO" + q)
+
+# age = int(input())
+# print(age)
+# del age
+
+# age_2 = 42
+# print("His age is" + str(age_2))
+
+# print("Dawid")
+
+# name = input()
+# age = input()
+
+# print(name + "is" + age)
+
+print()
+x = x + 3
+x += 3
+print(x)
+
+w = "spam"
+print(w)
+
+w += "eggs"
+print(w)
+
+print (8 % 2)
+
+list = [2, 3, 4, 5, 6, 7]
+
+for x in list:
+    if(x%2==1 and x>4):
+        print(x)
+        break
